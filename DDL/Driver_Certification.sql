db9340c5a2b20bb34f4e0e2ed1981af980c6eeb7
Datpham112 <104989653@student.swin.edu.au>
2026-06-28 16:24:48 +0700

    Add files via upload

---
+++ DDL/Driver_Certification.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE Driver_Certification (
+    Driver_ID INT NOT NULL,
+    Certification_Name VARCHAR(50) NOT NULL,
+    Expiry_Date DATE NOT NULL,
+
+    PRIMARY KEY (Driver_ID, Certification_Name),
+
+    CONSTRAINT fk_cert_driver
+        FOREIGN KEY (Driver_ID)
+        REFERENCES Driver(Driver_ID)
+);
+    
