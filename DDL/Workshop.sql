daa7d34a1ee9d6f846dd215875d09c1c1597d541
Dulee27 <duongle271006@gmail.com>
2026-07-02 22:27:24 +0700

    Create Workshop table with foreign key constraint

---
+++ DDL/Workshop.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Workshop (
+    Workshop_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Depot_ID INT NOT NULL,
+
+    CONSTRAINT fk_workshop_depot
+        FOREIGN KEY (Depot_ID)
+        REFERENCES Depot(Depot_ID)
+);
