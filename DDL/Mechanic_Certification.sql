1648daf2ce0fc339570a26c499afa20a0b2bb90e
Dulee27 <duongle271006@gmail.com>
2026-07-02 22:29:18 +0700

    Create Mechanic_Certification table with constraints

---
+++ DDL/Mechanic_Certification.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Mechanic_Certification (
+    Mechanic_ID INT NOT NULL,
+    Certification_Name VARCHAR(100) NOT NULL,
+    Issue_Date DATE NOT NULL,
+    Expiry_Date DATE NOT NULL,
+
+    PRIMARY KEY (Mechanic_ID, Certification_Name),
+
+    CONSTRAINT fk_mechanic_certification
+        FOREIGN KEY (Mechanic_ID)
+        REFERENCES Mechanic(Mechanic_ID),
+
+    CONSTRAINT chk_mechanic_certificate
+        CHECK (
+            Certification_Name IN (
+                'Standard Automotive Mechanic',
+                'EV Technician',
+                'Refrigeration System Certification'
+            )
+        ),
+
+    CONSTRAINT chk_mechanic_dates
+        CHECK (Expiry_Date > Issue_Date)
+);
