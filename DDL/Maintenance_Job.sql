20332b4649cc0f6616b9a79cd2462d621126e8b7
Dulee27 <duongle271006@gmail.com>
2026-07-13 09:14:50 +0700

    Add check constraint for job dates in Maintenance_Job

--- DDL/Maintenance_Job.sql
+++ DDL/Maintenance_Job.sql
@@ -7,22 +7,25 @@ CREATE TABLE Maintenance_Job (
     Date_Closed DATE,
     Downtime_Hours DECIMAL(6,2),
     Total_Cost DECIMAL(10,2),
-
+ 
     CONSTRAINT fk_job_vehicle
         FOREIGN KEY (VIN)
         REFERENCES Vehicle(VIN),
-
+ 
     CONSTRAINT fk_job_workshop
         FOREIGN KEY (Workshop_ID)
         REFERENCES Workshop(Workshop_ID),
-
+ 
     CONSTRAINT fk_job_alert
         FOREIGN KEY (Linked_Alert_ID)
         REFERENCES Predictive_Alert(Alert_ID),
-
+ 
     CONSTRAINT chk_downtime
         CHECK (Downtime_Hours >= 0),
-
+ 
     CONSTRAINT chk_total_cost
-        CHECK (Total_Cost >= 0)
+        CHECK (Total_Cost >= 0),
+ 
+    CONSTRAINT chk_job_dates
+        CHECK (Date_Closed IS NULL OR Date_Closed >= Date_Opened)
 );
