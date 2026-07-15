a2c07e60ebe2689cd01de87bb50a6c7518775d65
Dulee27 <duongle271006@gmail.com>
2026-07-15 20:17:56 +0700

    Insert predictive alert data into Predictive_Alert table

---
+++ DML/predictive_alert_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Predictive_Alert
+(
+  Vin,
+  Depot_ID,
+  Alert_Type,
+  Action_Taken
+)
+VALUES
+('VIN00000000000001', 1, 'Brake Wear', 'Scheduled Repair'),
+('VIN00000000000002', 1, 'Tyre Pressure', 'Acknowledged'),
+('VIN00000000000003', 2, 'Battery Degradation', 'Emergency Repair'),
+('VIN00000000000004', 3, 'Overheating Risk', 'Resolved'),
+('VIN00000000000005', 4, 'Engine Fault', 'Acknowledged');
+ 
+
+  
