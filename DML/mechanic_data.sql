80f514dde3c67f0d9fdbb4a637e0fa8787f5859a
Dulee27 <duongle271006@gmail.com>
2026-07-15 11:46:07 +0700

    Insert mechanic data into Mechanic table

---
+++ DML/mechanic_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Mechanic
+(
+  Workshop_ID,
+  Full_Name
+)
+VALUES
+(1, 'Nguyen Van Hung'),
+(1, 'Tran Van Duc'),
+(2, 'Le Thi Mai'),
+(3, 'Pham Van Son'),
+(3, 'Hoang Thi Lan'),
+(4, 'Vo Van Tam');
