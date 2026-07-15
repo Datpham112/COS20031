62394892526bd6f51f5c56a6b69ca72cf46d1c8b
Dulee27 <duongle271006@gmail.com>
2026-07-15 11:57:57 +0700

    Insert initial data into Part_Supplier table
    Insert multiple records into the Part_Supplier table with details including Part_ID, Supplier_ID, Supplier_Type, Unit_Cost, and Lead_Time_Days.

---
+++ DML/part_supplier_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Part_Supplier
+(
+  Part_ID,
+  Supplier_ID,
+  Supplier_Type,
+  Unit_Cost,
+  Lead_Time_Days
+)
+VALUES
+(1, 1, 'Primary', 380000.00, 3),
+(1, 4, 'Backup', 400000.00, 5),
+(2, 4, 'Primary', 2900000.00, 7),
+(3, 2, 'Primary', 23000000.00, 14),
+(4, 3, 'Primary', 1600000.00, 5),
+(5, 1, 'Primary', 100000.00, 2),
+(6, 1, 'Primary', 75000.00, 2);
