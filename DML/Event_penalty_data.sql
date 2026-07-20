eaa0bd3f034b4b17e78a609539a68bd29ec5e0a8
Datpham112 <104989653@student.swin.edu.au>
2026-07-20 15:23:43 +0700

    Add files via upload

---
+++ DML/Event_penalty_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Event_Penalty
+(Event_Type, Penalty_Points)
+VALUES
+('Speeding', 10),
+('Harsh Braking', 8),
+('Rapid Acceleration', 5),
+('Fatigue Driving', 20),
+('Phone Usage', 15),
+('Seatbelt Violation', 10),
+('Lane Departure', 12),
+('Collision Warning', 25),
+('Unsafe Following Distance', 8),
+('Engine Overspeed', 6);
