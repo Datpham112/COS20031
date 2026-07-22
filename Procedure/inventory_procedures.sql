4fccc166cdc5a499113b5291cef2d21730161f5c
Dulee27 <duongle271006@gmail.com>
2026-07-22 09:14:58 +0700

    Implement trigger for auto-creating warranty claims
    Added a trigger to automatically create warranty claims after inserting into Activity_Part.

--- Procedure/inventory_procedures.sql
+++ Procedure/inventory_procedures.sql
@@ -1,8 +1,5 @@
--- 3: STORED PROCEDURES 
-
 DELIMITER //
 
--- -> [Warranty_Claim]
 CREATE PROCEDURE sp_Register_Warranty_Claim(
     IN p_Activity_ID INT,
     IN p_Part_ID INT,
@@ -25,7 +22,6 @@ BEGIN
     );
 END //
 
--- -> [Part], [Supplier], [Part_Supplier]
 CREATE PROCEDURE sp_Get_Optimal_Supplier(
     IN p_Part_Name_Keyword VARCHAR(100)
 )
@@ -42,6 +38,39 @@ BEGIN
     JOIN Supplier s ON ps.Supplier_ID = s.Supplier_ID
     WHERE p.Part_Name LIKE CONCAT('%', p_Part_Name_Keyword, '%')
     ORDER BY ps.Lead_Time_Days ASC, ps.Unit_Cost ASC;
+END //
+
+DELIMITER ;
+
+-- Triggers
+DELIMITER //
+
+CREATE TRIGGER trg_Auto_Create_Warranty_Claim
+AFTER INSERT ON Activity_Part
+FOR EACH ROW
+BEGIN
+    DECLARE is_warranty BOOLEAN;
+
+    SELECT Warranty_Indicator INTO is_warranty
+    FROM Maintenance_Activity
+    WHERE Activity_ID = NEW.Activity_ID;
+
+    IF is_warranty = TRUE THEN
+        INSERT INTO Warranty_Claims (
+            Activity_ID, 
+            Part_ID, 
+            Claim_Status, 
+            Claim_Date, 
+            Claim_Type
+        )
+        VALUES (
+            NEW.Activity_ID, 
+            NEW.Part_ID, 
+            'Pending', 
+            CURDATE(), 
+            'Auto-Generated (System)'
+        );
+    END IF;
 END //
 
 DELIMITER ;
