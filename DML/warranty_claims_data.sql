5ec5335fc6052070ec345485893937b144599f69
NguyenHoangLong <105977950@student.swin.edu.au>
2026-07-23 09:34:49 +0700

    add 2 db

---
+++ DML/warranty_claims_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Warranty_Claims
+(
+  Activity_ID,
+  Part_ID,
+  Claim_Status,
+  Claim_Date,
+  Claim_Type
+)
+VALUES
+(1, 1, 'Approved', '2024-02-18', 'Part Replacement'),
+(2, 2, 'Pending', '2024-04-05', 'Tyre Warranty'),
+(3, 3, 'Approved', '2024-06-21', 'Battery Warranty'),
+(4, 4, 'Rejected', '2024-08-14', 'Cooling System Repair'),
+(5, 5, 'Pending', '2024-10-02', 'Oil Filter Replacement')
