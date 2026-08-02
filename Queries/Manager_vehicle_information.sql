d4d9ae9d343142a3db1bb16a4f3e3ffc3cf500cc
Datpham112 <104989653@student.swin.edu.au>
2026-08-02 18:23:24 +0700

    use case 1

---
+++ Queries/Manager_vehicle_information.sql
@@ -0,0 +1,10 @@
+UPDATE Vehicle
+SET
+    Registration_Number = '29A12345',
+    Vehicle_Category = 'Heavy Truck',
+    Manufacturer_and_Model = 'Volvo FH16',
+    Year_of_Manufacture = 2024,
+    Current_Odometer = 128500.50,
+    Depot_ID = 2,
+    Operational_Status = 'Active'
+WHERE VIN = 'VIN00000000000001';
